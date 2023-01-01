b61b39429257d25109f1f865e3f6d745213a2c63
HtonFirst <al.kozlov.rus@gmail.com>
2023-01-01 16:16:46 +0300

    first commit S-Pb

---
+++ src/main/java/com/example/buysell/services/ProductService.java
@@ -0,0 +1,80 @@
+package com.example.buysell.services;
+
+import com.example.buysell.models.Image;
+import com.example.buysell.models.Product;
+import com.example.buysell.repositories.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+
+    public List<Product> getProductList(String title) {
+        if (title != null) {
+            return productRepository.findByTitle(title);
+        }
+
+        return productRepository.findAll();
+    }
+
+    public void saveProduct(Product product, MultipartFile file1, MultipartFile file2, MultipartFile file3) {
+        Image image1;
+        Image image2;
+        Image image3;
+
+        if (file1.getSize() != 0) {
+            image1 = toImageEntity(file1);
+            image1.setPreviewImage(true);
+        }
+        if (file1.getSize() != 0) {
+            image2 = toImageEntity(file2);
+        }
+        if (file1.getSize() != 0) {
+            image3 = toImageEntity(file3);
+        }
+
+
+        log.info("saving new {}", product);
+        productRepository.save(product);
+    }
+
+    public void deleteProduct(Long id) {
+        productRepository.deleteById(id);
+    }
+
+    public Product getProductById(Long id) {
+        return productRepository.findById(id).orElse(null);
+    }
+
+
+    private Image toImageEntity(MultipartFile file) {
+        try {
+            Image image = new Image();
+            image.setName(file.getName());
+            image.setOriginalFileName(file.getOriginalFilename());
+            image.setContentType(file.getContentType());
+            image.setSize(file.getSize());
+            image.setBytes(file.getBytes());
+
+            return image;
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+}
